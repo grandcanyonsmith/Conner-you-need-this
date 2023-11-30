6ad23163d1072c421e508e1186f127ed57d017a2
Canyon Smith <canyonfsmith@gmail.com>
2023-11-29 21:46:57 -0700

    Updated main.py!

---
+++ main.py
@@ -0,0 +1,12 @@
+# New Code for saying "Hello, World!" and printing it
+
+# Function definition
+def say_hello():
+    # Create a message
+    message = "Hello, World!"
+    # Print the message
+    print(message)
+
+# Main program execution
+if __name__ == "__main__":
+    say_hello()
