6edcede25948e696ca9ff81b67829f406427b89a
Canyon Smith <canyonfsmith@gmail.com>
2023-12-11 13:37:41 -0700

    Updated aws_test.py!

--- aws_test.py
+++ aws_test.py
@@ -1,42 +1,47 @@
 import boto3
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timezone
 
 # Initialize a DynamoDB service client with the us-west-2 region
 dynamodb = boto3.client('dynamodb', region_name='us-west-2')
 
-# Function to retrieve a list of all DynamoDB tables and their last updated timestamps
-def fetch_tables_and_update_info():
-    # Create a list to store info about each table
+# Function to retrieve a list of all DynamoDB tables and their creation timestamps
+def fetch_tables_and_creation_info():
     all_tables_info = []
-    # Make the initial request to list tables
     response = dynamodb.list_tables()
-    
-    # Loop through all tables to accumulate their information
+
     while True:
-        # For every table listed in the response, describe it to get details
         for table_name in response.get('TableNames', []):
             description = dynamodb.describe_table(TableName=table_name)
-            # Extract the LastUpdateTime, fall back to January 1, 2000 if not available
-            last_updated = description['Table'].get('LastUpdateTime', datetime(2000, 1, 1))
-            all_tables_info.append({'TableName': table_name, 'LastUpdated': last_updated})
-        
-        # Handle response pagination if there are more tables
-        if 'LastEvaluatedTableName' in response:
-            response = dynamodb.list_tables(ExclusiveStartTableName=response['LastEvaluatedTableName'])
-        else:
-            break  # No more tables left, exit the loop
-    
-    return all_tables_info  # Return the list of tables and their last updated timestamps or the default date
+            # Extract the CreationDateTime
+            creation_time_utc = description['Table']['CreationDateTime']
+            all_tables_info.append({
+                'TableName': table_name,
+                'CreatedAt': creation_time_utc
+            })
 
-# Function to convert the list of DynamoDB table information into a Pandas DataFrame
-def convert_to_dataframe():
-    tables_info = fetch_tables_and_update_info()
+        if 'LastEvaluatedTableName' in response:
+            response = dynamodb.list_tables(
+                ExclusiveStartTableName=response['LastEvaluatedTableName'])
+        else:
+            break
+
+    # Sort tables by creation date
+    sorted_tables_info = sorted(all_tables_info, key=lambda x: x['CreatedAt'])
+    return sorted_tables_info
+
+# Function to convert the list of DynamoDB table information into a Pandas DataFrame, including formatting the creation date
+def convert_to_dataframe(tables_info):
+    # Format creation dates
+    for table in tables_info:
+        table['CreatedAt'] = table['CreatedAt'].strftime('%b. %d %Y %I:%M %p')
+
+    # Convert to DataFrame
     dataframe = pd.DataFrame(tables_info)
     return dataframe
 
 # Execute the main script
 if __name__ == "__main__":
-    # Create the DataFrame and print it out
-    tables_dataframe = convert_to_dataframe()
+    tables_info = fetch_tables_and_creation_info()
+    tables_dataframe = convert_to_dataframe(tables_info)
     print(tables_dataframe)
